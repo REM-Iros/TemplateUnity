315d2f2d9563d3a6c2790ec1104707b181437577
REM-Iros <remiros910@gmail.com>
2024-11-10 17:25:42 -0700

    Working on managers, investigating dynamic injection

--- Assets/Scripts/Utils/Audio Manager.cs
+++ Assets/Scripts/Utils/Audio Manager.cs
@@ -1,16 +1,88 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+/// <summary>
+/// Basic audio manager to be used throughout a game. A library of all of the sound effects and
+/// audio will be stored, and then the game objects will make a call to the audio manager to play
+/// those sound effects.
+/// 
+/// REM-i
+/// </summary>
+public class AudioManager<T> : MonoBehaviour where T : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region Vars
+
+    //Create an audio clip dictionary that will be called
+    private Dictionary<string, AudioClip> audioDict = new Dictionary<string, AudioClip>();
+
+    #endregion
+
+    #region Callable Methods
+
+    public void PlayAudio(string audioName)
     {
-        
+        if(audioDict.ContainsKey(audioName))
+        {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    #region Singleton Stuff
+
+    //Instance variable that stores manager as an instance
+    private static T _instance;
+
+    //Lock to prevent multithread issues
+    private static readonly object _lock = new object();
+
+    //Create instance that we can call from other scripts
+    public static T Instance
     {
-        
+        get
+        {
+            //Lock down manager so others can use it
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    //Look for already instantiated instance
+                    _instance = FindAnyObjectByType<T>();
+
+                    //If no instance, then create a new one
+                    if (_instance == null)
+                    {
+                        GameObject singletonObject = new GameObject(typeof(T).Name);
+                        _instance = singletonObject.AddComponent<T>();
+
+                        //Make manager persistant
+                        DontDestroyOnLoad(singletonObject);
+                    }
+                }
+                return _instance;
+            }
+        }
     }
+
+    /// <summary>
+    /// Awake method to check if there is an audiomanager already present, if not, set
+    /// this as the audio manager singleton.
+    /// </summary>
+    protected virtual void Awake()
+    {
+        //Check for instance
+        if (_instance == null)
+        {
+            //Set instance if null
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        //Otherwise we have a dupe and destroy it
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
 }
--- Assets/Scripts/Utils/Game Manager.cs
+++ Assets/Scripts/Utils/Game Manager.cs
@@ -1,16 +1,60 @@
 using UnityEngine;
-
-public class GameManager : MonoBehaviour
+/// <summary>
+/// Basic gamemanager script. This will load on startup, and will provide basic features
+/// needed to get the game running.
+/// 
+/// REM-i
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class GameManager<T> : MonoBehaviour where T : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private static T _instance;
+    private static readonly object _lock = new object();
+
+    /// <summary>
+    /// Create an instance that we can reference in script
+    /// </summary>
+    public static T Instance
     {
-        
+        get
+        {
+            //Lock to prevent multithread issues
+            lock (_lock)
+            {
+                //If the instance isn't set, find it
+                if (_instance == null)
+                {
+                    _instance = FindAnyObjectByType<T>();
+
+                    //If we can't find one, create this as the manager singleton
+                    if (_instance == null)
+                    {
+                        GameObject singletonObject = new GameObject(typeof(T).Name);
+                        _instance = singletonObject.AddComponent<T>();
+
+                        //Set this manager as persistant
+                        DontDestroyOnLoad(singletonObject);
+                    }
+                }
+                return _instance;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Called on startup, 
+    /// </summary>
+    protected virtual void Awake()
     {
-        
+        // Ensure there is only one instance of the Singleton
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject); // Optional
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject); // Destroy duplicate instance
+        }
     }
 }
