f849fa9c2a28bcb285a100e1958bf178442afdda
REM-Iros <remiros910@gmail.com>
2025-03-18 21:34:56 -0600

    Adding more implementation to the rebind button script

--- Assets/Scripts/UI/Buttons/Button Actions/Specific Actions/Rebinding/Rebind Control.cs
+++ Assets/Scripts/UI/Buttons/Button Actions/Specific Actions/Rebinding/Rebind Control.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using TMPro;
 
@@ -20,17 +21,32 @@ public class RebindControl : ButtonBase
     [SerializeField, Header("Input Action Variables")]
     private InputActionReference _actionRef;
 
+    // This var is the rebind operation to rebind the set binding
+    private InputActionRebindingExtensions.RebindingOperation _rebindOp;
+
     #endregion
 
     #region UI Vars
 
-    [Tooltip("This is the panel gameobject that appears when we start rebinding")]
+    [Tooltip("This is the text that displays the current binding of the kbm control")]
     [SerializeField, Header("UI Variables")]
+    private TextMeshProUGUI _kbmBindingText;
+
+    [Tooltip("This is the image that displays the current binding of the gamepad control")]
+    [SerializeField]
+    private Image _gamePadBindingImage;
+
+    #region Panel Vars
+
+    [Tooltip("This is the panel gameobject that appears when we start rebinding")]
+    [SerializeField]
     private GameObject _panel;
 
     [Tooltip("This is the text component we modify to display the binding")]
     [SerializeField]
-    private TextMeshProUGUI _rebindText;
+    private TextMeshProUGUI _rebindPanelText;
+
+    #endregion
 
     #endregion
 
@@ -38,6 +54,9 @@ public class RebindControl : ButtonBase
 
     #region Methods
 
+    /// <summary>
+    /// When the button is pressed, we want to rebind the control if possible.
+    /// </summary>
     protected override void OnButtonPressed()
     {
         // Check that we actually have an action and throw an error if not
@@ -50,7 +69,94 @@ public class RebindControl : ButtonBase
         // Disable the current action
         _actionRef.action.Disable();
 
+        // Update the UI to indicate we are ready to rebind
+        if (_panel != null && _rebindPanelText != null)
+        {
+            _panel.SetActive(true);
+            _rebindPanelText.text = "Rebinding: " + _actionRef.action.name + " (Press any key)";
+        }
+        
 
+        // Perform any operations we need to before we rebind
+        _rebindOp = _actionRef.action.PerformInteractiveRebinding()
+            // Disallow mouse controls
+            .WithControlsExcluding("<Mouse>")
+            // Enable cancelling via select and escape
+            .OnPotentialMatch(operation =>
+            {
+                if (operation.selectedControl.path == "/Keyboard/escape")
+                {
+                    operation.Cancel();
+                    return;
+                }
+
+                // This is simply a test line
+                Debug.Log(operation.selectedControl.path);
+
+                if (operation.selectedControl.path == "/Gamepad/select")
+                {
+                    operation.Cancel();
+                    return;
+                }
+            })
+            // On Cancel, we need to reset the operation
+            .OnCancel(operation => CancelRebinding())
+            // On Complete, we need to cleanup the rebindings
+            .OnComplete(operation =>
+            {
+                if( _panel != null )
+                {
+                    _panel.SetActive(false);
+                }
+
+                CleanupRebinding();
+            });
+
+        // Start the rebind
+        _rebindOp.Start();
+    }
+
+    /// <summary>
+    /// Call this method when the rebind is cancelled
+    /// </summary>
+    private void CancelRebinding()
+    {
+        // Disable the panel if present and cancel the rebind
+        if (_rebindOp != null && _rebindOp.started)
+        {
+            if (_panel != null)
+            {
+                _panel.SetActive(false);
+            }
+
+            _rebindOp?.Cancel();
+
+            // Reset the operation
+            CleanupRebinding();
+        }
+    }
+
+    /// <summary>
+    /// Call this method whenever the rebind ends, either cancel or completion. 
+    /// Reset the operation and enable the action.
+    /// </summary>
+    private void CleanupRebinding()
+    {
+        // Clean up the operation
+        _rebindOp?.Dispose();
+        _rebindOp = null;
+
+        _actionRef.action.Enable();
+    }
+
+    /// <summary>
+    /// This method exists only if the dev wants to include resetting the current
+    /// input action, instead of resetting all inputs, needs to be called by another button
+    /// </summary>
+    public void ResetInputAction()
+    {
+        ////TODO: Need to get the proper binding index for each of the actions
+        _actionRef.action.RemoveBindingOverride();
     }
 
     #endregion
